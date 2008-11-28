815bec7ee088e4decd809d29fd0a099b7511c9ed
uwiger <uwiger@5caf71e4-375c-0410-90e7-deec3ad9e8da>
2008-11-28 15:35:54 +0000

    reorg
    git-svn-id: http://svn.ulf.wiger.net/erl2latex@4 5caf71e4-375c-0410-90e7-deec3ad9e8da

--- trunk/erl2latex.erl
+++ trunk/erl2latex.erl
@@ -1,4 +1,8 @@
-%% The MIT License
+%% \title{erl2latex: Literal Erlang Programming}
+%% \maketitle
+%%
+%% Copyright (c) 2008 Ulf Wiger, John Hughes\footnote{
+%% \tiny{The MIT License
 %%
 %% Copyright (c) 2008 Ulf Wiger <ulf@wiger.net>,
 %%                    John Hughes <john.hughes@quviq.com>
@@ -20,36 +24,61 @@
 %% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
 %% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 %% DEALINGS IN THE SOFTWARE.
+%% }}
+
+%% \section{Introduction}
+%% This module converts an Erlang source file to latex. The latex file
+%% can then be converted to e.g. PDF, using pdflatex or similar tool.
+%%
+%% The idea of `literal Erlang programming' is that the source and comments
+%% should read as a good paper. Unlike XML markup, Latex markup is also 
+%% fairly unobtrusive when reading the source directly.
 %%
 -module(erl2latex).
 
 -export([file/1, file/2]).
 
-
+%% \section{file/[1,2]}
+%% 
+%% The interface is:\\
+%% file(Filename [, Target\_filename]) -> ok | \{error, Reason\}
+%%
+%% If no target filename is given, the .erl extension of the source filename
+%% will be stripped and replaced with .tex.
+%%
 file(F) ->
     file(F, latex_target(F)).
 
 file(F, Target) ->
     case file:read_file(F) of
         {ok, Bin} ->
-            Parts = split_input(binary_to_list(Bin)),
-            Latex =
-                case binary_to_list(
-                       list_to_binary([convert_part(P) || P <- Parts])) of
-                    "\\document" ++ _ = Latex0 ->
-                        {Preamble,Doc} = get_preamble(Latex0),
-                        [Preamble, source_listing_setup(Preamble),
-                         Doc, end_doc()];
-                    Latex0 ->
-                        [default_preamble(),
-                         "\\begin{document}\n",
-                         Latex0, end_doc()]
-                end,
-            output(Latex, Target);
+            output(convert_to_latex(Bin), Target);
         Err ->
             Err
     end.
 
+%% The actual conversion function. We separate comments from code,
+%% and convert each block to latex separately. We then insert a preamble,
+%% if not already present, or insert a small formatting macro for the 
+%% source code (if not already defined).
+%%
+convert_to_latex(Bin) ->
+    Parts = split_input(binary_to_list(Bin)),
+    case lists:flatten([convert_part(P) || P <- Parts]) of
+        "\\document" ++ _ = Latex0 ->
+            {Preamble,Doc} = get_preamble(Latex0),
+            [Preamble, source_listing_setup(Preamble),
+             Doc, end_doc()];
+        Latex0 ->
+            [default_preamble(),
+             "\\begin{document}\n",
+             Latex0, end_doc()]
+    end.
+
+
+%% If a preamble is present, the $\backslash$begin{document} entry
+%% must also be present. This is how we know where the preamble ends.
+%%
 get_preamble(Str) ->
     get_preamble(Str, []).
 
@@ -87,8 +116,18 @@ end_doc() ->
 
 
 split_input(Txt) ->
-    Lines = string:tokens(Txt, "\n"),
-    group([wrap(L) || L <- Lines]).
+    group([wrap(L) || L <- lines(Txt)]).
+
+lines(Str) ->
+    lines(Str, []).
+
+lines("\n" ++ Str, Acc) ->
+    [lists:reverse(Acc) | lines(Str,[])];
+lines([H|T], Acc) ->
+    lines(T, [H|Acc]);
+lines([], Acc) ->
+    [lists:reverse(Acc)].
+    
 
 wrap("%" ++ S) ->
     {comment, strip_comment(S)};
@@ -102,45 +141,60 @@ group([{T,C}|Tail]) ->
 group([]) ->
     [].
 
-
-
-
-
-
+%% In this function, we wrap the different `source' and `comment' blocks
+%% appropriately. The weird-looking split between string parts is to keep
+%% pdflatex from tripping on what looks like the end of the verbatim block.
+%%
 convert_part({code,Lines}) ->
     ["\\begin{mylisting}\n"
      "\\begin{verbatim}\n",
      [[expand(L),"\n"] || L <- Lines],
-     "\\end{verbatim}\n"
+     "\\" "end{verbatim}\n"
      "\\end{mylisting}\n\n"];
 convert_part({comment,Lines}) ->
     [[[L,"\n"] || L <- Lines],"\n"].
+
+
+%% The expand(Line) function expands tabs for better formatting. The
+%% tab expansion algorithm is really too simplistic.
 
 expand(Line) ->
     lists:map(fun($\t) -> ["    "];
                  (C) -> C
               end, Line).
-                       
 
-escape(Lines) ->
-    [[[escape_char(C) || C <- L],"\n"] || L <- Lines].
+%% Following edoc convention, comments are excluded if the first non-space
+%% character following the leading string of \% is another \%,
+%% for example:\\
+%% \%\% \% This comment will be excluded.
 
-escape_char(C) ->
-    case lists:member(C, "#$%&_{}") of
-        true ->
-            [$\\,C];
-        false ->
-            if C==$\\ -> "\\char92";
-               C==$\^ -> "\\char94";
-               C==$~  -> "\\char126";
-               true -> C
-            end
-    end.
+%% % escape_char(C) ->
+%% %     case lists:member(C, "#$%&_{}") of
+%% %         true ->
+%% %             [$\\,C];
+%% %         false ->
+%% %             if C==$\\ -> "\\char92";
+%% %                C==$\^ -> "\\char94";
+%% %                C==$~  -> "\\char126";
+%% %                true -> C
+%% %             end
+%% %     end.
 
 strip_comment(C) ->
-    string:strip(lists:dropwhile(fun($\%) -> true; (_) -> false end, C), left).
+    C1 = strip_percents(C),
+    case string:strip(C1, left) of
+        "%" ++ _ -> "";
+        Stripped ->
+            Stripped
+    end.
 
+strip_percents("%" ++ C) ->
+    strip_percents(C);
+strip_percents(C) ->
+    C.
 
+%% Finally, just a few utility functions.
+%%
 latex_target(F) ->
     filename:basename(F,".erl") ++ ".tex".
 
